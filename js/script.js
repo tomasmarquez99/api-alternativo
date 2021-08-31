d47b4f913a75c4f0292eb131e72c4f4debdb06a6
tomasmarquez99 <btomasmarquez@gmail.com>
2021-08-31 10:46:14 -0300

    boostrap

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@ let ropa_URL = "https://tomasmarquez99.github.io/api-alternativo/ropa.json";
 function cargarDatos(datos) {
     let ropa = "";
     for (let i = 0; i < datos.length; ++i) {
-        
+
         let datosRopa = datos[i];
 
         nombres = datosRopa.nombre;
         precios = datosRopa.precio;
         imagenes = datosRopa.imagen;
 
-        document.getElementById("productos").innerHTML += ` ${nombres} ${precios} <img src="${imagenes}"> <br><br>`;
+        document.getElementById("productos").innerHTML +=
+        `  
+        <tr>
+        <td>${nombres}</td>
+        <td>$${precios}</td>
+        <td><img src="${imagenes}" class="float-right img-fluid"></td>
+        </tr>
+        `;
 
-      
-       
+
+
     }
-    
+
 
 }
 
