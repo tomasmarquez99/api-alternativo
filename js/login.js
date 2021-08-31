70445a555f47495876ad0a52ae0fe7d8b38b8799
tomasmarquez99 <btomasmarquez@gmail.com>
2021-08-30 22:29:11 -0300

    Login y comienzo de boostrap

--- js/login.js
+++ js/login.js
@@ -1,3 +1,26 @@
-function redirrecion(){
-location.href="index.html"
-}
+
+document.addEventListener("DOMContentLoaded", function(){
+
+document.getElementById('submition').addEventListener('click', function() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let camposCompletos = true;
+
+
+    if (email.value === '') {
+        camposCompletos = false
+    }
+
+    if (password.value === '') {
+        camposCompletos = false
+    } 
+
+    if (camposCompletos){
+        window.location = "index.html"
+    }
+
+})
+
+localStorage.setItem('usuario', email.value);
+
+})
